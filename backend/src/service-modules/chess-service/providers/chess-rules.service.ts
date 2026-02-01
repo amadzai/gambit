309d68e7ee6fe63f4450ca02ec351f77c0289172
amadzai <20966134+amadzai@users.noreply.github.com>
2026-02-02 00:11:43 +0800

    feat: Add create game

--- backend/src/service-modules/chess-service/providers/chess-rules.service.ts
+++ backend/src/service-modules/chess-service/providers/chess-rules.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Chess } from 'chess.js';
+import { PrismaService } from '../../prisma/prisma.service.js';
+import { Color, GameStatus } from '../../../../generated/prisma/client.js';
 
 @Injectable()
-export class ChessRulesService {}
+export class ChessRulesService {
+  constructor(private prisma: PrismaService) {}
+
+  async createGame() {
+    const game = new Chess();
+
+    return this.prisma.chessGame.create({
+      data: {
+        fen: game.fen(),
+        turn: Color.WHITE,
+        status: GameStatus.ACTIVE,
+      },
+    });
+  }
+}
