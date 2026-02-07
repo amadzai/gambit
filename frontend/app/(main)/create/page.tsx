02307b91b0c7492b7a129b8d7733c7727c2c85f0
amadzai <20966134+amadzai@users.noreply.github.com>
2026-02-07 16:45:12 +0800

    chore: Delete unused page

--- frontend/app/(main)/create/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CreateAgentForm } from "@/components/create/create-agent-form";
-
-export default function CreatePage() {
-  return (
-    <div className="container mx-auto px-4 py-16">
-      <CreateAgentForm />
-    </div>
-  );
-}
